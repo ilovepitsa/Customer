f0f2e8fa792208739a07456dbc30cdd29e22cb2b
ilovepitsa <90710107+ilovepitsa@users.noreply.github.com>
2024-02-20 19:29:20 +0300

    add Test Handler

---
+++ api/handlers/customerHandlers.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type CustomerHandler struct {
+	l *log.Logger
+}
+
+func NewCustomerHandler(l *log.Logger) *CustomerHandler {
+	return &CustomerHandler{l: l}
+}
+
+func (ch *CustomerHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "helloFromCustomer")
+	ch.l.Println("Get all Customers")
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	handlers "github.com/ilovepitsa/Customer/api/handlers/customerHandlers.go"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	custHandl := handlers.NewCustomerHandler(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/customers", custHandl)
+
+	http.ListenAndServe(":8080", sm)
 }
